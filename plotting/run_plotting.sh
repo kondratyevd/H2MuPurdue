90090ba099300481146f92b385e0f327c375c616
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-17 13:58:31 -0400

    fix

--- plotting/run_plotting.sh
+++ plotting/run_plotting.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test/ 
-mkdir $PLOTTING_OUT_PATH
+mkdir -p $PLOTTING_OUT_PATH
 python $FRAMEWORK_PATH/plotting/scripts/test_hammer.py --out_path $PLOTTING_OUT_PATH
 # python $FRAMEWORK_PATH/plotting/scripts/test_local.py --out_path $PLOTTING_OUT_PATH
