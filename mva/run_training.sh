cda7188fffed83f08d9f1308b0aaa13a7433e9bb
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-23 17:57:35 -0400

    Add more options

--- mva/run_training.sh
+++ mva/run_training.sh
@@ -10,18 +10,33 @@ case $1 in
 	0)
 		echo 'Running option 0'
 		python $FRAMEWORK_PATH/mva/scripts/test_dnn_local.py --out_path $OUTPUT_DIR
+		echo "Deleting output dir... (test run)"
+		rm -rf $OUTPUT_DIR
 		;;
 
 	1)
 		echo 'Running option 1'
 		python $FRAMEWORK_PATH/mva/scripts/test_hammer.py --out_path $OUTPUT_DIR
+		echo "Deleting output dir... (test run)"
+		rm -rf $OUTPUT_DIR
 		;;
 
+	2.0)
+		echo 'Running option 2.0: DNN inclusive'
+		python $FRAMEWORK_PATH/mva/scripts/dnn_ucsd_inclusive.py --out_path $OUTPUT_DIR
+		;;
+	2.1)
+		echo 'Running option 2.1: DNN 01jet'
+		python $FRAMEWORK_PATH/mva/scripts/dnn_ucsd_01jet.py --out_path $OUTPUT_DIR
+		;;
+	2.2)
+		echo 'Running option 2.2: DNN 2jet bveto'
+		python $FRAMEWORK_PATH/mva/scripts/dnn_ucsd_2jet_bveto.py --out_path $OUTPUT_DIR
+		;;
 	*)
-		echo 'Wrong option ' $1;;		
-
+		echo 'Wrong option ' $1		
+		echo "Deleting output dir... (test run)"
+		rm -rf $OUTPUT_DIR;;
 esac
 
-echo "Deleting output dir... (test run)"
-rm -rf $OUTPUT_DIR
 
