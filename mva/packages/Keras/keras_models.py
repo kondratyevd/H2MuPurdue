5375637ea392e50045b8cac2dbece0a1adb5cd9d
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-31 15:03:32 -0400

    fixed input layer size

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1234,7 +1234,7 @@ def GetListOfModels(trainer):
         return - tf.reduce_sum(target * tf.log(output), -1)
 
 
-    model_resweights = model_init('model_resweights', input_dim, 2048, 100, [categorical_crossentropy_resweights], 'adam')
+    model_resweights = model_init('model_resweights', input_dim+1, 2048, 100, [categorical_crossentropy_resweights], 'adam')
     x = Dense(50, name = model_resweights.name+'_layer_1', activation='relu')(model_resweights.inputs)
     x = Dropout(0.2)(x)
     x = Dense(25, name = model_resweights.name+'_layer_2', activation='relu')(x)
