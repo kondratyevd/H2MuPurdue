64bbace353d8359dd54533064f32351d0cbc8b26
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-12 10:56:55 -0400

    fix

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1303,7 +1303,7 @@ def GetListOfModels(trainer):
             log1 = (s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB+K.epsilon())+K.epsilon()
             log2 = 1+sigB*sigB*s/(b*(b+sigB*sigB)+K.epsilon())
 
-            result = 1./(2*((s+b)*K.log(log1)-b*b*K.log(log2[0])/(sigB*sigB+K.epsilon()))) #Add the epsilon to avoid dividing by 0
+            result = 1./(2*((s+b)*K.log(log1[0])-b*b*K.log(log2[0])/(sigB*sigB+K.epsilon()))) #Add the epsilon to avoid dividing by 0
             return tf.convert_to_tensor(result[0])
 
         return asimovSigLossInvert
