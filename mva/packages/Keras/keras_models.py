fc3c0c49546fb243c472d19f4c7e955b6b61547c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-18 15:17:49 -0400

    0.003

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1272,13 +1272,13 @@ def GetListOfModels(trainer):
         return sigLossInvert
 
     model_sigloss = model_init('model_sigloss', input_dim, 4096, 30, [significanceLossInvert(trainer.expectedS, trainer.expectedB)], 'adam')
-    x = Dense(50, name = model_sigloss.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.00301))(model_sigloss.inputs)
+    x = Dense(50, name = model_sigloss.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.003))(model_sigloss.inputs)
     x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss.name+'_layer_2', activation='relu')(x)
     # x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss.name+'_layer_3', activation='relu')(x)
     # x = Dropout(0.2)(x)
-    model_sigloss.outputs = Dense(1, name = model_sigloss.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.00301))(x)
+    model_sigloss.outputs = Dense(1, name = model_sigloss.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.003))(x)
 
     list_of_models.append(model_sigloss)
 
@@ -1305,13 +1305,13 @@ def GetListOfModels(trainer):
         return sigLossMultiInvert
 
     model_sigloss_multi = model_init('model_sigloss_multi', input_dim, 4096, 30, [significanceLossMultiInvert(trainer.expected_counts, trainer.signal_mask)], 'adam')
-    x = Dense(50, name = model_sigloss_multi.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.00301))(model_sigloss_multi.inputs)
+    x = Dense(50, name = model_sigloss_multi.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.003))(model_sigloss_multi.inputs)
     x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss_multi.name+'_layer_2', activation='relu')(x)
     # x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss_multi.name+'_layer_3', activation='relu')(x)
     # x = Dropout(0.2)(x)
-    model_sigloss_multi.outputs = Dense(1, name = model_sigloss_multi.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.00301))(x)
+    model_sigloss_multi.outputs = Dense(1, name = model_sigloss_multi.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.003))(x)
 
     list_of_models.append(model_sigloss_multi)
 
