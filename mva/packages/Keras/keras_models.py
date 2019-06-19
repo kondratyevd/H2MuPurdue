0047dbc22d96fec3cd493f4e80e15993e3740d6b
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-19 15:47:54 -0400

    test

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1302,7 +1302,7 @@ def GetListOfModels(trainer):
 
             result = (s+b)/(s*s+K.epsilon()) #Add the epsilon to avoid dividing by 0
             # return tf.convert_to_tensor(result[0])
-            return s
+            return tf.convert_to_tensor(s[0])
 
         return sigLossMultiInvert
 
