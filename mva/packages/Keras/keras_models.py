cbb49bfa174c814f5d0954d986bccf67ca40f5a7
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-19 15:58:03 -0400

    test

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1292,17 +1292,17 @@ def GetListOfModels(trainer):
         def sigLossMultiInvert(y_true,y_pred):
             s = 0
             b = 0
+            test = K.sum(y_true[:, 0])
+            # for i in range(len(expected_counts)):
+            #     if signal_mask[i]:      #signal
+            #         s = s + expected_counts[i]/K.sum(y_true[:, i])*K.sum(y_pred*y_true[:, i])
+            #     else:                   # background
+            #         b = b + expected_counts[i]/K.sum(y_true[:, i])*K.sum(y_pred*y_true[:, i])
 
-            for i in range(len(expected_counts)):
-                if signal_mask[i]:      #signal
-                    s = s + expected_counts[i]/K.sum(y_true[:, i])*K.sum(y_pred*y_true[:, i])
-                else:                   # background
-                    b = b + expected_counts[i]/K.sum(y_true[:, i])*K.sum(y_pred*y_true[:, i])
 
-
-            result = (s+b)/(s*s+K.epsilon()) #Add the epsilon to avoid dividing by 0
+            # result = (s+b)/(s*s+K.epsilon()) #Add the epsilon to avoid dividing by 0
             # return tf.convert_to_tensor(result[0])
-            return tf.convert_to_tensor(s[0])
+            return tf.convert_to_tensor(test[0])
 
         return sigLossMultiInvert
 
