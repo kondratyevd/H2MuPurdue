53bc538355e837b5c8be792dc9199a41823bffa2
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-19 09:59:12 -0400

    test

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1305,7 +1305,7 @@ def GetListOfModels(trainer):
 
         return sigLossMultiInvert
 
-    model_sigloss_multi = model_init('model_sigloss_multi', input_dim, 4096, 30, [significanceLossMultiInvert(trainer.expected_counts, trainer.signal_mask)], 'adam')
+    model_sigloss_multi = model_init('model_sigloss_multi', input_dim, 4096, 10, [significanceLossMultiInvert(trainer.expected_counts, trainer.signal_mask)], 'adam')
     x = Dense(50, name = model_sigloss_multi.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.003))(model_sigloss_multi.inputs)
     x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss_multi.name+'_layer_2', activation='relu')(x)
