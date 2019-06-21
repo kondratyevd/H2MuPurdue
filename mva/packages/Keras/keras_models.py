511b3ab029ce3ffbe52c49fd5fb12e2df6d9c9bb
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-21 09:50:01 -0400

    l=0.001

--- mva/packages/Keras/keras_models.py
+++ mva/packages/Keras/keras_models.py
@@ -1273,13 +1273,13 @@ def GetListOfModels(trainer):
         return sigLossInvert
 
     model_sigloss = model_init('model_sigloss', input_dim, 4096, 30, [significanceLossInvert(trainer.expectedS, trainer.expectedB)], 'adam')
-    x = Dense(50, name = model_sigloss.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.002))(model_sigloss.inputs)
+    x = Dense(50, name = model_sigloss.name+'_layer_1', activation='relu', kernel_regularizer=l2(0.001))(model_sigloss.inputs)
     x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss.name+'_layer_2', activation='relu')(x)
     # x = Dropout(0.2)(x)
     # x = Dense(25, name = model_sigloss.name+'_layer_3', activation='relu')(x)
     # x = Dropout(0.2)(x)
-    model_sigloss.outputs = Dense(1, name = model_sigloss.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.002))(x)
+    model_sigloss.outputs = Dense(1, name = model_sigloss.name+'_output',  activation='sigmoid', kernel_regularizer=l2(0.001))(x)
 
     list_of_models.append(model_sigloss)
 
