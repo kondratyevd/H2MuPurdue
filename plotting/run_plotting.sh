77e00df6ec8526e494b0a6845047521967fffce6
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-08-21 16:27:03 -0400

    fix syntax

--- plotting/run_plotting.sh
+++ plotting/run_plotting.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
-SCRIPT_PATH = $FRAMEWORK_PATH/plotting/scripts/test_latinos.py
+SCRIPT_PATH=$FRAMEWORK_PATH/plotting/scripts/test_latinos.py
 
 case $1 in
 	0.0)
 		echo 'Running option 0.0: Zpeak, no ZpT reweighting'
 		PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_Zpeak_uncorr/ 
-		ZPT = false
-		MASS_MIN = 70
-		MASS_MAX = 110
+		ZPT=false
+		MASS_MIN=70
+		MASS_MAX=110
 		;;
 
 	0.1)
 		echo 'Running option 0.1: Zpeak, ZpT reweighting'
 		PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_Zpeak_corr/ 
-		ZPT = true
-		MASS_MIN = 70
-		MASS_MAX = 110
+		ZPT=true
+		MASS_MIN=70
+		MASS_MAX=110
 		;;
 
 	1.0)
 		echo 'Running option 1.0: fit region, no ZpT reweighting'
 		PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_fitreg_uncorr/ 
-		ZPT = false
-		MASS_MIN = 110
-		MASS_MAX = 150
+		ZPT=false
+		MASS_MIN=110
+		MASS_MAX=150
 		;;
 
 	1.1)
 		echo 'Running option 1.1: fit region, ZpT reweighting'
 		PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_fitreg_corr/ 
-		ZPT = true
-		MASS_MIN = 70
-		MASS_MAX = 110
+		ZPT=true
+		MASS_MIN=70
+		MASS_MAX=110
 		;;
 	*)
 		echo 'Wrong option ' $1		
