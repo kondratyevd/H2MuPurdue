e2ce5cc04180fb14c74002426dd3aaa9f7614cda
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-08-21 17:05:42 -0400

    fix jet selection

--- plotting/scripts/test_latinos.py
+++ plotting/scripts/test_latinos.py
@@ -16,7 +16,6 @@ parser.add_argument('--mass_max', action='store', dest='mass_max', help='Mass ma
 args = parser.parse_args()
 p = NTuplePlotter()
 p.reweight_zpt = args.zpt
-print args.zpt
 p.tree_path = "Events"
 p.has_metadata = False
 
@@ -92,8 +91,9 @@ supercut = '   Lepton_pt[0]>26 \
             && (Lepton_pdgId[0]*Lepton_pdgId[1]==(-13*13)) \
             '
 bveto = '(Sum$(CleanJet_pt > 20. && abs(CleanJet_eta)<2.5 && Jet_btagDeepB[CleanJet_jetIdx] > 0.6321) == 0)'
-jets = '((!CleanJet_pt[0] || CleanJet_pt[0]>30) && (!CleanJet_pt[1] || CleanJet_pt[1]>30) )'
-
+jets = '(nCleanJet==0) || ((nCleanJet>0) && (CleanJet_pt[0]>30)) || ((nCleanJet>1) && (CleanJet_pt[0]>30) && (CleanJet_pt[1]>30))'
+# jets = '((!CleanJet_pt[0] || CleanJet_pt[0]>30) && (!CleanJet_pt[1] || CleanJet_pt[1]>30) )'
+# jets = 1
 selection = '(mll>%s)&(mll<%s)&(%s)&(%s)&(%s)'%(args.mass_min, args.mass_max, supercut, bveto, jets) # Z peak
 
 
