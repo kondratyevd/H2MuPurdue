d846fa9d9483195da74d038b647b5c1ee544474c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-08-21 14:43:45 -0400

    ptll without correction

--- plotting/run_plotting.sh
+++ plotting/run_plotting.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_fitreg_uncorr/
-PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_Zpeak_corr/ 
+PLOTTING_OUT_PATH=$FRAMEWORK_PATH/plotting/plots/test_latinos_Zpeak_uncorr/ 
 mkdir -p $PLOTTING_OUT_PATH
 # python $FRAMEWORK_PATH/plotting/scripts/test_hammer.py --out_path $PLOTTING_OUT_PATH
 python $FRAMEWORK_PATH/plotting/scripts/test_latinos.py --out_path $PLOTTING_OUT_PATH
--- plotting/scripts/test_latinos.py
+++ plotting/scripts/test_latinos.py
@@ -12,7 +12,7 @@ parser.add_argument('--out_path', action='store', dest='output_path', help='Outp
 args = parser.parse_args()
 
 p = NTuplePlotter()
-p.reweight_zpt = True
+# p.reweight_zpt = True
 p.tree_path = "Events"
 p.has_metadata = False
 
@@ -66,10 +66,10 @@ p.add_data_dir(latinos_data_2016.name, latinos_data_2016.path+files_to_run, lati
 # p.add_variable("yll")
 # p.add_variable("cosThetaCS")
 # p.add_variable("phiCS")
-# p.add_variable("ptll")
+p.add_variable("ptll")
 # p.add_variable("Muon_pt", 2)
 # p.add_variable("Muon_eta", 2)
-p.add_variable("CleanJet_pt", 2)
+# p.add_variable("CleanJet_pt", 2)
 # p.add_variable("CleanJet_eta", 2)
 # p.add_variable("detajj")
 # p.add_variable("mjj")
